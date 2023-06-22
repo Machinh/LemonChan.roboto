8066e9cb66a262e195a8c1713c52ffd85524c722
Machine <68440743+Machinh@users.noreply.github.com>
2023-06-22 15:15:32 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,18 @@
 import discord
 from discord.ext import commands
 
-# Configuração do bot
-token = ''
-prefixo = '!'
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
 
-# Criação do bot
-bot = commands.Bot(command_prefix=prefixo)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Evento de inicialização do bot
 @bot.event
 async def on_ready():
     print(f'Bot conectado como {bot.user.name}')
 
-# Comando básico
 @bot.command()
 async def ola(ctx):
     await ctx.send(f'Olá, {ctx.author.mention}!')
 
-# Comando de repetição
-@bot.command()
-async def repetir(ctx, *, mensagem):
-    await ctx.send(mensagem)
-
-# Execução do bot
-bot.run(token)
+bot.run('SEU_TOKEN_AQUI')
